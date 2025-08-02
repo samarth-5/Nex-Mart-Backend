0acb9398ed871b8e702d181b35cccb0f03deda2c
Samarth Narayan <kayaniv321@gmail.com>
2025-08-02 19:20:43 +0530

    intialised spring

---
+++ src/main/java/com/samarth_dev/NexMartApplication.java
@@ -0,0 +1,13 @@
+package com.samarth_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samarth_dev/NexMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samarth_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
