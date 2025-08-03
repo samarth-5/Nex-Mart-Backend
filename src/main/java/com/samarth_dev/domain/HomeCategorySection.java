ebbecbda47f384cecf6c30a8471ade808bccdf3a
Samarth Narayan <kayaniv321@gmail.com>
2025-08-03 12:04:37 +0530

    completed all modal classes schema

---
+++ src/main/java/com/samarth_dev/domain/HomeCategorySection.java
@@ -0,0 +1,8 @@
+package com.samarth_dev.domain;
+
+public enum HomeCategorySection {
+    ELECTRIC_CATEGORIES,
+    GRID,
+    SHOP_BY_CATEGORIES,
+    DEALS
+}
---
+++ src/main/java/com/samarth_dev/modal/HomeCategory.java
@@ -0,0 +1,28 @@
+package com.samarth_dev.modal;
+
+import com.samarth_dev.domain.HomeCategorySection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HomeCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String image;
+    private String categoryId;
+    private HomeCategorySection section;
+}
